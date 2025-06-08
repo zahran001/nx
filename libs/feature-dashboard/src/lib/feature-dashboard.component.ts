e916736d1c0f41d42dd0acef74d3073d702f546b
Zahran Yahia Khan <114562123+zahran001@users.noreply.github.com>
2025-06-07 22:07:36 -0400

    added feature library - dashboard

---
+++ libs/feature-dashboard/src/lib/feature-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-feature-dashboard',
+  imports: [CommonModule],
+  template: `<p>FeatureDashboard works!</p>`,
+  styles: ``,
+})
+export class FeatureDashboardComponent {}
